78f30e05c0f089f9117f6d88ed3241c783400fbe
roy.dammarell <roy.dammarell@logos.com>
2016-03-23 09:25:00 -0700

    Fix off-by-one click error

--- Common.Droid/Views/HeaderAdapter.cs
+++ Common.Droid/Views/HeaderAdapter.cs
@@ -31,22 +31,45 @@ namespace Common.Droid.Views
 			}
 		}
 
+		public override int GetPosition(object item)
+		{
+			int position = base.GetPosition(item);
+
+			if (HeaderTemplateId != 0 && position != -1)
+				position++;
+
+			return position;
+		}
+
+		public override object GetRawItem(int position)
+		{
+			object item;
+			if (HeaderTemplateId == 0)
+				item = base.GetRawItem(position);
+			else if (position == 0)
+				item = m_bindingContext;
+			else
+				item = base.GetRawItem(position - 1);
+
+			return item;
+		}
+
 		protected override View GetView(int position, View convertView, ViewGroup parent, int templateId)
 		{
-			if (m_headerView != null && m_headerView == convertView)
+			if (m_headerView == convertView)
 				convertView = null;
-			
+
 			View view;
 			if (HeaderTemplateId == 0)
 				view = base.GetView(position, convertView, parent, templateId);
 			else if (position == 0)
 				view = m_headerView ?? (m_headerView = m_bindingContext.BindingInflate(HeaderTemplateId, null));
 			else
-				view = base.GetView(position - 1, convertView, parent, templateId);
+				view = base.GetView(position, convertView, parent, templateId);
 
 			return view;
 		}
-			
+
 		readonly IMvxAndroidBindingContext m_bindingContext;
 		View m_headerView;
 	}
